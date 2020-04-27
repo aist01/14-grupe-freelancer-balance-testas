09079cdf9789b943386035baea1c10f8a7edf665
el Aiste <aiste.norei@gmail.com>
2020-04-27 10:23:23 +0300

    Men pavadinimai

---
+++ js/table.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const menesiai = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Geguze', 'Birzelis', 'Liepa', 
+                    'Rugpjutis', 'Rugsejis', 'Spalis', 'Lapkritis', 'Gruodis'];
